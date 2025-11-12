785fe0edab3b8e8d166f4eac8d314c08bd7f11aa
nawam123 <nawam@pinnacloeducare.com>
2025-11-12 21:54:54 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -1,25 +1,32 @@
-import express from "express";
-import cors from "cors";
+import express from "express"
+import cors from "cors"
 
-const app = express();
+const app=express()
 
-app.use(express.json());
+app.use(express.json())
 
-app.use(cors({
-  origin: [
+app.use(cors(
+{origin:
+    [
+    "http://localhost:5172",
     "http://localhost:5173",
     "http://localhost:3000",
-    "http://38.137.5.56:5173",   // Your public frontend
-    "http://38.137.5.56",         // Add this too
-  ],
-  credentials: true,
-}));
+    // add production url
+    ],
+    credentials: true,
+    // methods: ["GET","POST", "PUT","PATCH","DELETE"]
+}
+))
 
-app.get("/api/message", (req, res) => {
-  res.json({ message: "Hlo From Nawam" });
+// Api 
+app.get("/api/message",(req,res)=>{
+    res.json({message:"Hlo From Nawam"})
 });
 
-const PORT = 4000;
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`✅ Server running at http://localhost:${PORT}`);
-});
+// Port Defining
+const PORT = 4000
+
+// Port Activating
+app.listen(PORT,"0.0.0.0",()=>console,console.log(`Server is Running at
+ http://localhost:${PORT}`));
+
