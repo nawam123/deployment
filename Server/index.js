acd1afb3924066dac23012f660d7e793cb24c9bf
nawam123 <nawam@pinnacloeducare.com>
2025-11-12 23:14:00 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -1,32 +1,36 @@
-import express from "express"
-import cors from "cors"
+// server/server.js
 
-const app=express()
+import express from "express";
+import cors from "cors";
 
-app.use(express.json())
+const app = express();
 
-app.use(cors(
-{origin:
-    [
-    "http://localhost:5172",
-    "http://localhost:5173",
-    "http:////38.137.5.56:3000",
-    // add production url
+app.use(express.json());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173", // Good to keep for local testing
+      "http://localhost:5174",
+      "http://localhost:3000",
+      "http://localhost:443",
+      "http://38.137.5.56:443",
+       "http:////38.137.5.56:3000",
+        // <-- ADD THIS LINE
     ],
     credentials: true,
-    // methods: ["GET","POST", "PUT","PATCH","DELETE"]
-}
-))
+  })
+);
 
-// Api 
-app.get("/api/message",(req,res)=>{
-    res.json({message:"Hlo From Nawam"})
+// API route
+app.get("/api/message", (req, res) => {
+  res.json({ message: "Hello from chaicode server" });
 });
 
-// Port Defining
-const PORT = 4000
+const PORT = 4000;
+app.listen(PORT, "0.0.0.0", () =>
+  console.log(`Server is running at http://localhost:${PORT}`)
+);
 
-// Port Activating
-app.listen(PORT,"0.0.0.0",()=>console,console.log(`Server is Running at
- http://localhost:${PORT}`));
+
+
 
