3380b10a602c7c5e4b7f4c4fdc0afebbba091e31
nawam123 <nawam@pinnacloeducare.com>
2025-11-12 21:55:43 +0530

    Update App.tsx

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -4,10 +4,9 @@ import "./App.css";
 function App() {
   const [message, setMessage] = useState("");
 
-  const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:4000";
 
   useEffect(() => {
-    fetch("http://http://38.137.5.56:4000/api/message")
+    fetch("http://38.137.5.56//:4000/api/message")
       .then(res => res.json())
       .then(data => setMessage(data.message))
       .catch(err => console.error("Error fetching message:", err));
