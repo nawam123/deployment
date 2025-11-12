4a7cf4177a000c872bcc4a9c8f794701cecaf757
nawam123 <nawam@pinnacloeducare.com>
2025-11-12 17:52:24 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -1,36 +1,3 @@
-// import express from "express"
-// import cors from "cors"
-
-// const app=express()
-
-// app.use(express.json())
-
-// app.use(cors(
-// {origin:
-//     [
-//     "http://localhost:5172",
-//     "http://localhost:5173",
-//     "http://localhost:3000",
-//     // add production url
-//     ],
-//     credentials: true,
-//     // methods: ["GET","POST", "PUT","PATCH","DELETE"]
-// }
-// ))
-
-// // Api 
-// app.get("/api/message",(req,res)=>{
-//     res.json({message:"Hlo From Nawam"})
-// });
-
-// // Port Defining
-// const PORT = 4000
-
-// // Port Activating
-// app.listen(PORT,"0.0.0.0",()=>console,console.log(`Server is Running at
-//  http://localhost:${PORT}`));
-
-
 import express from "express";
 import cors from "cors";
 
@@ -42,7 +9,8 @@ app.use(cors({
   origin: [
     "http://localhost:5173",
     "http://localhost:3000",
-    "http://38.137.5.56:5173",   // public frontend
+    "http://38.137.5.56:5173",   // Your public frontend
+    "http://38.137.5.56",         // Add this too
   ],
   credentials: true,
 }));
